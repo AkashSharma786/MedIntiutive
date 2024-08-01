1aa39a94bb473903556605b75d1793f2ad053f4d
Akash Sharma <akashsharma91795@gmail.com>
2024-08-01 12:07:16 +0530

    just checking

--- lib/pages/store_management/orders_page.dart
+++ lib/pages/store_management/orders_page.dart
@@ -1,53 +1,42 @@
 import 'package:flutter/material.dart';
 
 class OrdersPage extends StatefulWidget {
-
   const OrdersPage({super.key});
 
   @override
   State<OrdersPage> createState() => OrdersPageState();
-  
 }
 
 class OrdersPageState extends State<OrdersPage> {
-
   int currentClickedButton = 0;
-  void updateColumn(int num)
-  {
+  void updateColumn(int num) {
     setState(() {
       currentClickedButton = num;
     });
-    
-  
   }
+
   @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     return Column(
-
       children: [
-
         Container(
           width: screenSize.width,
           height: 50,
           color: Colors.deepPurple,
-          child: Text("$currentClickedButton",),
+          child: Text(
+            "$currentClickedButton",
+          ),
         ),
-
         Container(
           width: screenSize.width,
-          height: screenSize.height-146,
+          height: screenSize.height - 146,
           color: Colors.pink,
-          child: Text("Orders Page",),
+          child: Text(
+            "Orders Page",
+          ),
         )
-
-
       ],
-
-
     );
-      
-
-    
   }
 }
